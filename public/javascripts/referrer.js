73bb58aea31b06c2a7a53fda6e7d3a669d90e5e5
Juan Maiz <juanmaiz@gmail.com>
2010-09-30 17:55:27 -0400

    referrer

---
+++ public/javascripts/referrer.js
@@ -0,0 +1,17 @@
+function getParameter( name )
+{
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( window.location.href );
+  if( results == null )
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+referrer = getParameter('referrer')
+
+if(referrer) {
+	$.cookie('referrer', referrer, { expires: 30 })
+}
