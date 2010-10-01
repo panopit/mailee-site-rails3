8f7ead91b4ed677e7d06b91debea0f7026bc4701
Juan Maiz <juanmaiz@gmail.com>
2010-10-01 11:03:18 -0400

    bot done

--- cabral_bot.rb
+++ cabral_bot.rb
@@ -1,48 +1,33 @@
 require 'rubygems'
 require 'percy'
 
-@online = false
+AWAY_MESSAGE = "Ninguém da Softa está online neste momento. Por favor, encaminhe suas dúvidas para o Cabral no email *suporte@mailee.me*."
+configure{|c| YAML.load_file('config/irc.yml').each{|k,v| c.send("#{k}=", v) } }
 
-conf = YAML.load('config/irc.yml')
+def softas; users_on('#maileeme') & %w{cabral leotartari joaomilho diobob danielw pedroaxl} end
+def online; ! softas.empty? end
 
-configure do |c|
-  c.nick     = "cabral_robot"
-  c.username = 'Robô assistente do Cabral'
-  c.server   = "irc.freenode.net"
-  c.password = "maileebotsofta"
-  c.port     = 6667
-end
+on(:connect){ join "#maileeme" }
 
-def softas
-  users_on('#maileeme') & NICKS
-end
-
-on :connect do
-  join "#maileeme"
-end
-
-NICKS = %w{cabral leotartari joaomilho diobob danielw pedroaxl}
 on [:join,:quit,:nickchange] do |env|
-  @online = ! softas.empty?
-  message env[:channel], "Verificando... tem alguém da Softa online? #{@online ? 'Sim' : 'Não'}"
-  File.open('online.txt','w'){|f| f.write @online.to_s }
+  message env[:channel], "Verificando... tem alguém da Softa online? #{online ? 'Sim' : 'Não'}"
+  File.open('online.txt','w'){|f| f.write online.to_s }
 end
 
-#on :channel do |env|
-#  msg "O Cabral não está. Por favor, mande um e-mail para ele em suporte@mailee.me"
-#end
-on [:channel] do |env|  
-  unless @online
-    message env[:channel], "Ninguém da Softa está online neste momento. Por favor, encaminhe suas dúvidas para o Cabral no email."
-  end
+on :channel do |env|  
+  message env[:channel], AWAY_MESSAGE unless online
 end
 
-on [:query] do |env|  
-  if @online
-    message env[:nick], "Eu sou só um robô. Por favor, fale com o #{softas.first}."
-    message softas.first, "Hey! #{env[:nick]} precisa de suporte!"
+on :query do |env|
+  if users_on('#maileeme').include?(env[:nick])
+    if online
+      message env[:nick], "Eu sou só um robô. Por favor, fale com o #{softas.first}."
+      message softas.first, "Hey! #{env[:nick]} precisa de suporte!"
+    else
+      message env[:nick], AWAY_MESSAGE
+    end
   else
-    message env[:nick], "Ninguém da Softa está online neste momento. Por favor, encaminhe suas dúvidas para o Cabral no email."
+    message env[:nick], "Para ajuda com o Mailee.me entre na sala #maileeme (/join #maileeme)."
   end
 end
 
