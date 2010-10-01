87e19118b7e9a33418b1b4917c62a076b2715f64
Juan Maiz <juanmaiz@gmail.com>
2010-10-01 14:11:46 -0400

    little bug fixd

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -51,15 +51,12 @@ class SiteController < ApplicationController
     # %input{ :name => "list[]", :type => "hidden", :value => "22642" }
   end
   
+  # Sends the email via ajax.
   def contact
-    # Sends the email via ajax.
-    def contact
-      Site.contact(params).deliver
-      render :json => {:ok => true}.to_json
-    rescue 
-      render :json => {:ok => false}.to_json
-    end
-
+    Site.contact(params).deliver
+    render :json => {:ok => true}.to_json
+  rescue 
+    render :json => {:ok => false}.to_json
   end
   
 protected
